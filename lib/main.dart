a99d766e521acd6f8fbb9036474b1c529ef1209c
evan-mcgeek <evan.mcgeek@gmail.com>
2021-02-03 01:38:37 +0700

    changed button pressed appearance

--- lib/main.dart
+++ lib/main.dart
@@ -5,37 +5,41 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(DrumMachineApp());
 
 class DrumMachineApp extends StatelessWidget {
+  //Play sound function
   void playSound(int number, double volume) {
-    var player = AudioPlayer().setVolume(volume);
-    var cache = AudioCache(fixedPlayer: AudioPlayer());
+    AudioPlayer().setVolume(volume);
+    var cache = AudioCache();
     cache.play('$number.wav');
   }
 
   void stopSound() {
-    AudioPlayer().stop();
+    AudioPlayer()?.pause();
   }
 
   Expanded buildKey({Color color, int number, double volume, String name}) {
     return Expanded(
-      child: FlatButton(
-        padding: EdgeInsets.all(10),
-        onPressed: () => playSound(number, volume),
-        onLongPress: () => stopSound(),
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: color,
-            boxShadow: [
-              BoxShadow(
-                color: color.withOpacity(0.5),
-                spreadRadius: 2,
-                blurRadius: 5,
-                offset: Offset(3, 3), // changes position of shadow
-              ),
-            ],
-          ),
-          width: 200,
-          height: 200,
+      child: Container(
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: color,
+          boxShadow: [
+            BoxShadow(
+              color: color.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: Offset(3, 3), // changes position of shadow
+            ),
+          ],
+        ),
+        width: 200,
+        height: 200,
+        child: FlatButton(
+          splashColor: Colors.white24,
+          onPressed: () {
+            playSound(number, volume);
+          },
+          onLongPress: () => stopSound(),
           child: Center(
             child: Text(
               name,
@@ -54,6 +58,9 @@ class DrumMachineApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.black,
@@ -74,7 +81,7 @@ class DrumMachineApp extends StatelessWidget {
                       buildKey(
                           color: Colors.orange,
                           number: 2,
-                          volume: 0.7,
+                          volume: 0.5,
                           name: 'SNARE'),
                     ],
                   ),
@@ -103,7 +110,7 @@ class DrumMachineApp extends StatelessWidget {
                       buildKey(
                           color: Colors.teal,
                           number: 5,
-                          volume: 0.6,
+                          volume: 0.3,
                           name: 'MELODY 1'),
                       buildKey(
                           color: Colors.blue,
